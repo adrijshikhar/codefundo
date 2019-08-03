06a8dd9ff19b399b2b3c97ea5485253a431add35
Aviral Gangwar <aviralgangwar24@gmail.com>
2019-08-03 11:11:10 +0530

    Dummy candidates

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,11 +1,25 @@
 pragma solidity ^0.5.8;
 
 contract Election {
-    //Store Candidate
-    //Read Candidate
-    string public candidate;
-    // Constructor
-    constructor () public {
-        candidate = "Candidate 1";
+    //Model a candidate
+    struct Candidate{
+        uint id;
+        string name;
+        uint voteCount;
     }
+
+    //Store Candidates
+    //Fetch Candidates
+    mapping(uint => Candidate) public candidates;
+    // Store Candidates Count
+    uint public candidatesCount;
+
+    constructor () public {
+        addCandidate("Candidate 1");
+        addCandidate("Candidate 2");
+    }
+
+    function addCandidate (string memory _name) private {
+        candidatesCount ++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);}
 }
